7aa2808591c792b15aa5f32dbf922d808acb036c
pbrehill <pbrehill@gmail.com>
2025-11-04 13:11:09 +1100

    Added bibliography

---
+++ _bibliography/.auctex-auto/papers.el
@@ -0,0 +1,25 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "papers"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "biddle2022mental"
+    "noble2023wellbeing"
+    "biddle2022wellbeing"
+    "biddle2022relationship"
+    "fisk2020gender"
+    "rehill2025policy"
+    "crawford2021guidance"
+    "edwards2022variation"
+    "rehill2023fairness"
+    "rehill2024transparency"
+    "rehill2024distilling"
+    "taylor2023process"
+    "rehill2024heterogeneous"
+    "rehill2025applied"
+    "rehill2025understanding"
+    "rehill2025randomised"
+    "suomi2025family"))
+ '(or :bibtex :latex))
+
